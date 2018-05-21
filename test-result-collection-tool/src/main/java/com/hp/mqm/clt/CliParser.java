016db70342363b8ec494cadc10124e3c45d703fd
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-05-21 10:37:17 +0300

    tech : increment version

--- test-result-collection-tool/src/main/java/com/hp/mqm/clt/CliParser.java
+++ test-result-collection-tool/src/main/java/com/hp/mqm/clt/CliParser.java
@@ -40,7 +40,7 @@ public class CliParser {
     private static final String CMD_LINE_SYNTAX = "java -jar test-result-collection-tool.jar [OPTIONS]... FILE [FILE]...\n";
     private static final String HEADER = "HP Lifecycle Management Test Result Collection Tool";
     private static final String FOOTER = "";
-    private static final String VERSION = "1.0";
+    private static final String VERSION = "1.0.1";
 
     private Options options = new Options();
     private LinkedList<String> argsWithSingleOccurrence = new LinkedList<String>();
