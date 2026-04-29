332e3865ce60f3c7502610bfc4f4dff0c4b2886d
Ovidiu Stefan Popescu <opopescu@opentext.com>
2026-04-29 16:38:53 +0300

    user story #3130040: Support bearer token for test collection tool

--- test-result-collection-tool/src/main/java/com/microfocus/mqm/clt/authentication/AuthenticationMethod.java
+++ test-result-collection-tool/src/main/java/com/microfocus/mqm/clt/authentication/AuthenticationMethod.java
@@ -4,10 +4,19 @@ import com.microfocus.mqm.clt.Settings;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.cookie.Cookie;
 
 public interface AuthenticationMethod {
     HttpPost getLoginRequest(Settings settings);
 
     Cookie handleCookies(HttpRequest request, HttpResponse response);
+
+    default boolean isPreAuthenticated() {
+        return false;
+    }
+
+    default void addAuthorizationHeader(HttpUriRequest request, Settings settings) {
+        // No-op by default - cookie-based authentication methods don't need this
+    }
 }
